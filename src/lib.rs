f76b497299f4eaae6b630d62b9880b9e0b90716b
Nurudeen Abdul-Karim <nurudeen.karim2016@gmail.com>
2023-04-04 03:21:08 +0000

    handle files and skip bad files

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,69 @@
 use std::error::Error;
+use clap::{App,Arg,ArgGroup};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+#[derive(Debug)]
+pub struct Config{
+    files:Vec<String>,
+    number_lines:bool,
+    number_nonblank_lines:bool,
+}
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run()->MyResult<()>{
-    println!("Hello, world");
+// Function to get arguments
+pub fn get_args()->MyResult<Config>{
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("Nurudeen Abdul-Karim <nurudeen.karim2016@gmail.com>")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("Paths to files")
+                .default_value("-")
+                .min_values(1)
+        )
+        .arg(
+            Arg::with_name("number_lines")
+                .help("Number lines")
+                .takes_value(false)
+                .short("n")
+                .long("number")
+        )
+        .arg(
+            Arg::with_name("number_nonblank_lines")
+                .help("Number only non-blank lines")
+                .takes_value(false)
+                .short("b")
+                .long("number-nonblank")
+        )
+        .group(
+            ArgGroup::with_name("number")
+                .args(&["number_lines","number_nonblank_lines"])
+        ).get_matches();
+
+        Ok(Config{
+            files:matches.values_of_lossy("files").unwrap(),
+            number_lines:matches.is_present("number_lines"),
+            number_nonblank_lines:matches.is_present("number_nonblank_lines"),
+        })
+}
+
+fn open(filename:&str)->MyResult<Box<dyn BufRead>>{
+    match filename{
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn run(config:Config)->MyResult<()>{
+    for filename in config.files{
+        match open(&filename){
+            Err(err) => eprintln!("Failed to open {}: {}",filename,err),
+            Ok(_) => println!("Opened {}", filename),
+        }
+    }
     Ok(())
 }
---
+++ tests/cli.rs
@@ -0,0 +1,107 @@
+use std::fs;
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+fn run(args:&[&str], expected_file:&str)->TestResult{
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("catr")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
+fn fail(args:&[&str])->TestResult{
+    Command::cargo_bin("catr")?
+        .args(args)
+        .assert()
+        .failure();
+    Ok(())
+}
+
+// #[test]
+// fn skips_bad_file()->TestResult{
+//     fail(&["cant-touch-this"])
+// }
+
+#[test]
+fn empty()->TestResult{
+    run(&["tests/inputs/empty.txt"], "tests/expected/empty.txt")
+}
+
+#[test]
+fn emptyn()->TestResult{
+    run(&["-n","tests/inputs/empty.txt"], "tests/expected/empty.n.txt")
+}
+
+#[test]
+fn emptyb()->TestResult{
+    run(&["-b","tests/inputs/empty.txt"], "tests/expected/empty.b.txt")
+}
+
+
+#[test]
+fn fox()->TestResult{
+    run(&["tests/inputs/fox.txt"], "tests/expected/fox.txt")
+}
+
+#[test]
+fn foxn()->TestResult{
+    run(&["-n","tests/inputs/fox.txt"], "tests/expected/fox.n.txt")
+}
+
+#[test]
+fn foxb()->TestResult{
+    run(&["-b","tests/inputs/fox.txt"], "tests/expected/fox.b.txt")
+}
+
+
+#[test]
+fn spiders()->TestResult{
+    run(&["tests/inputs/spiders.txt"], "tests/expected/spiders.txt")
+}
+
+#[test]
+fn spidersn()->TestResult{
+    run(&["-n","tests/inputs/spiders.txt"], "tests/expected/spiders.n.txt")
+}
+
+#[test]
+fn spidersb()->TestResult{
+    run(&["-b","tests/inputs/spiders.txt"], "tests/expected/spiders.b.txt")
+}
+
+
+#[test]
+fn thebustle()->TestResult{
+    run(&["tests/inputs/the-bustle.txt"], "tests/expected/the-bustle.txt")
+}
+
+#[test]
+fn thebustlen()->TestResult{
+    run(&["-n","tests/inputs/the-bustle.txt"], "tests/expected/the-bustle.n.txt")
+}
+
+#[test]
+fn thebustleb()->TestResult{
+    run(&["-b","tests/inputs/the-bustle.txt"], "tests/expected/the-bustle.b.txt")
+}
+
+
+#[test]
+fn all()->TestResult{
+    run(&["tests/inputs/fox.txt","tests/inputs/spiders.txt","tests/inputs/the-bustle.txt"], "tests/expected/the-bustle.txt")
+}
+
+#[test]
+fn alln()->TestResult{
+    run(&["-n","tests/inputs/fox.txt","tests/inputs/spiders.txt","tests/inputs/the-bustle.txt"], "tests/expected/the-bustle.n.txt")
+}
+
+#[test]
+fn allb()->TestResult{
+    run(&["-b","tests/inputs/fox.txt","tests/inputs/spiders.txt","tests/inputs/the-bustle.txt"], "tests/expected/the-bustle.b.txt")
+}
